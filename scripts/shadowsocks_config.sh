446ca50a8e6d86322fb9eb489f0d9db4f658f4b4
indestinee <indestinee@gmail.com>
2018-02-17 13:40:28 +0000

    updated

--- scripts/shadowsocks_config.sh
+++ scripts/shadowsocks_config.sh
@@ -3,3 +3,4 @@ cd /etc/init.d
 sudo echo ssserver -c /etc/shadowsocks/config.json >> ss.sh
 chmod +x ss.sh
 sudo update-rc.d ss.sh defaults 95
+sudo ssserver -c /etc/shadowsocks/config.json -d start
