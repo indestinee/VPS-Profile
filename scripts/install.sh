936fb8258850cae3c3c3910d383a572c2342187b
indestinee <indestinee@gmail.com>
2018-06-03 13:43:47 +0000

    update

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,3 +5,6 @@ sudo apt install ssh -y
 sudo apt install vim-gnome -y
 sudo apt install shadowsocks -y
 
+sudo apt install python3 python3-distutils
+sudo apt install python3-pip
+sudo apt install make
