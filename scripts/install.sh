d0f4e9ee6440265f8c7863820df692f9abefb806
indestinee <indestinee@gmail.com>
2018-10-11 06:02:12 +0000

    update

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,7 +6,7 @@ sudo apt install vim-gnome -y
 sudo apt install shadowsocks -y
 sudo apt install ctags -y
 sudo apt install cmake -y
-
+sudo apt install zsh -y
 sudo apt install python3 python3-distutils -y
 sudo apt install python3-pip -y
 sudo apt install make -y
